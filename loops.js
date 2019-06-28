ef2fabf7b97d81f4b776fb16d383e07f11f5b7ad
merry-paradigm-9237 <k.merriman32@hotmail.com>
2019-06-28 16:39:59 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,13 +19,13 @@ function whileLoop(n) {
   return "done";
 }
 
-function doWhileLoop(array) {
+function doWhileLoop(num) {
   function maybeTrue() {
     return Math.random() >= 0.5
   }
 
   do {
-    array.pop();
+    num.pop();
   } while (array.length > -1 && maybeTrue());
     return array;
 }
