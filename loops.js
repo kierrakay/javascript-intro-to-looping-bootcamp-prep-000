063bad1549f336f7aec6bc314369842eeeb0d954
merry-paradigm-9237 <k.merriman32@hotmail.com>
2019-06-28 17:18:04 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,3 +19,13 @@ function whileLoop(n) {
   return "done";
 }
 
+var i = 0;
+ 
+function doWhileincrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("");
+} while (incrementVariable() < 5);
